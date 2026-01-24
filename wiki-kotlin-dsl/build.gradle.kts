34b9329501e0878ffcb6356e8808a1557b5b68f2
freya02 <41875020+freya022@users.noreply.github.com>
2026-01-24 13:07:16 +0100

    Remove unused deps

--- wiki-kotlin-dsl/build.gradle.kts
+++ wiki-kotlin-dsl/build.gradle.kts
@@ -12,6 +12,4 @@ dependencies {
     implementation(project(":wiki-commons"))
     implementation(project(":wiki-kotlin-commons"))
     implementation(libs.botcommands.jda.ktx)
-    implementation(libs.botcommands.typesafe.messages.core)
-    runtimeOnly(libs.botcommands.typesafe.messages.bc)
 }
