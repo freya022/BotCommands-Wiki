890c7382ebbfa67e43e2d88c0da47239e3565ed1
freya02 <41875020+freya022@users.noreply.github.com>
2026-07-10 13:36:06 +0200

    Remove project setup from root `build.gradle.kts`
    Was a leftover from before the modularization

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,47 +2,11 @@ import dev.freya02.gradle.tasks.DeployWikiTask
 import nl.littlerobots.vcu.plugin.resolver.VersionSelectors
 
 plugins {
-    `kotlin-conventions`
-
-    alias(libs.plugins.kotlinx.serialization)
-
     alias(libs.plugins.version.catalog.update)
 }
 
 versionCatalogUpdate {
     versionSelector(VersionSelectors.PREFER_STABLE)
-}
-
-dependencies {
-    // Coroutines
-    implementation(libs.kotlinx.coroutines.core)
-
-    // Discord stuff
-    implementation(libs.jda)
-    implementation(libs.botcommands)
-    implementation(libs.botcommands.jda.ktx)
-    implementation(libs.botcommands.spring)
-    implementation(libs.botcommands.typesafe.messages.core)
-    runtimeOnly(libs.botcommands.typesafe.messages.bc)
-
-    // Logging
-    implementation(libs.logback.classic)
-    implementation(libs.kotlin.logging)
-
-    // JSON
-    implementation(libs.kotlinx.serialization.json)
-
-    // SQL
-    runtimeOnly(libs.postgresql)
-    implementation(libs.flyway.core)
-    runtimeOnly(libs.flyway.postgresql)
-    implementation(libs.hikaricp)
-}
-
-kotlin.compilerOptions {
-    freeCompilerArgs.addAll(
-        "-opt-in=dev.freya02.botcommands.typesafe.messages.api.annotations.ExperimentalTypesafeMessagesApi",
-    )
 }
 
 val deployWiki = tasks.register<DeployWikiTask>("deployWiki") {
